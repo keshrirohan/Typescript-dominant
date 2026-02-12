f429486d2e7e4241cd7334ee433e4e06068a7239
keshrirohan <keshrirohan214@gmail.com>
2026-02-13 01:00:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 10;
+var b = 20;
+function add(x, y) {
+    return x + y;
+}
+add(a, b);
+//# sourceMappingURL=app.js.map
